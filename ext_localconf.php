a75195ca0c73785ab43abc455e23ce85c306350a
Frank Berger <fberger@b-factor.de>
2018-11-07 15:39:38 +0100

    fix path to backend preview

--- ext_localconf.php
+++ ext_localconf.php
@@ -13,13 +13,14 @@
 );
 
 
+/*
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['GLOBAL']['cliKeys']['migratettnews'] = array(
     'EXT:sudhaus7_newspage/Classes/Cli/Migratettnews.php',
     '_CLI_lowlevel'
 );
+*/
 
-
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem'][$_EXTKEY] = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Classes/Hooks/Backend/PreviewView.php:SUDHAUS7\Sudhaus7Newspage\Hooks\Backend\PreviewView';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem'][$_EXTKEY] = \SUDHAUS7\Sudhaus7Newspage\Hooks\Backend\PreviewView::class;
 
 
 
